3a2e31ca97ee2e22c7c1817e62ed9c822f44e437
pwkandt <123347213+pwkandt@users.noreply.github.com>
2023-01-22 23:45:44 -0500

    Create Exercise2.0.py

---
+++ Exercise2.0.py
@@ -0,0 +1,4 @@
+print("Parker Kandt\n")
+print("1/22/23"\n")
+print("At what size of a project should you start using version management software? Why?\n")
+print("The best time to use version management software, I believe, is when you're working on a code with even the smallest team of 2 or more people. If the project requires members of the team to work remotely away from each other, then it can not be argued that being able to collaborate at any given time is detrimental to the development of the code. I believe that it's good to use this software no matter what size code you're working on, as long as you're with a team of people, you should use whatever tools you can to work together")
